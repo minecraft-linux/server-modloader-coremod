6cd3ee5195c577eeb5c36250dc49e8f6ca6375f1
MCMrARM <mcmrarm@gmail.com>
2018-09-16 22:35:06 +0200

    Split main.cpp into subfiles

--- log_forwarder.cpp
+++ log_forwarder.cpp
@@ -1,33 +1,9 @@
 #include <modloader/statichook.h>
 #include <modloader/log.h>
-#include <modloader/loader.h>
 #include <string>
-#include <cstring>
 #include <cstdarg>
-#include <link.h>
-#include <dlfcn.h>
 
 using namespace modloader;
-
-THook(std::string, _ZN6Common22getServerVersionStringB5cxx11Ev) {
-  std::string ret = original();
-  ret += " modded (ModLoader ";
-  ret += ModLoader::getVersion();
-  bool f = true;
-  ModLoader::forEachMod([&ret, &f](void *v) {
-    struct link_map *ent;
-    if (dlinfo(v, RTLD_DI_LINKMAP, &ent) == 0 && ent != nullptr && ent->l_name != nullptr) {
-      if (!f)
-        ret += ", ";
-      else
-        ret += ", mods: ";
-      f = false;
-      ret += basename(ent->l_name);
-    }
-  });
-  ret += ")";
-  return ret;
-}
 
 class BedrockLog {
 public:
