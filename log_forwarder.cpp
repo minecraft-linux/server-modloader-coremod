c21c1c6181ee47c6d86d0de80bf40eb073caea20
Frago9876543210 <al.volf.av@gmail.com>
2019-07-31 12:41:07 +0300

    update log forwarder to 1.12

--- log_forwarder.cpp
+++ log_forwarder.cpp
@@ -1,15 +1,21 @@
 #include <modloader/statichook.h>
 #include <modloader/log.h>
 #include <string>
+#include <bitset>
 #include <cstdarg>
 
 using namespace modloader;
 
+enum LogAreaID : unsigned int {};
+
 class BedrockLog {
 public:
-  static const char* _areaFilterString(unsigned int);
+  static const char* _areaFilterString(LogAreaID);
 };
-TStaticHook(void, _ZN10BedrockLog7_log_vaEjjPKciS1_P13__va_list_tag, BedrockLog, unsigned int area, unsigned int level, char const* tag, int tid, char const* format, va_list args) {
+
+TStaticHook(void, _ZN10BedrockLog6log_vaENS_11LogCategoryESt6bitsetILm3EENS_7LogRuleE9LogAreaIDjPKciS6_P13__va_list_tag,
+            BedrockLog, unsigned int category, std::bitset<3> set, int rule, LogAreaID area, unsigned int level,
+            char const* tag, int tid, char const* format, va_list args) {
   Log::LogLevel ourLevel = MODLOADER_LOG_ERROR;
   if (level == 1)
     ourLevel = MODLOADER_LOG_TRACE;
